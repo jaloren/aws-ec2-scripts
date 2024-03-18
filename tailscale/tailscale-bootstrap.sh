47d8968c8f88125d67622a4dc552369f06b9586d
Joseph Lorenzini <jaloren@gmail.com>
2024-03-18 08:45:01 -0500

    refactoring to bootstrap tailscale with ssh on an ec2 instance

--- tailscale/tailscale-bootstrap.sh
+++ tailscale/tailscale-bootstrap.sh
@@ -3,64 +3,83 @@
 set -u
 set -o pipefail
 
-function log(){
-    local msg=$1
-    local logLevel=$2
-    local metadata
-    metadata=$(caller 1 | awk '{printf "lineno=%s funcName=%s",$1,$2}')
-    echo "[logLevel=${logLevel} ${metadata}] ${msg}"
+function log() {
+  local msg=$1
+  local logLevel=$2
+  local metadata
+  metadata=$(caller 1 | awk '{printf "lineno=%s funcName=%s",$1,$2}')
+  echo "[logLevel=${logLevel} ${metadata}] ${msg}"
 }
 
-function logErr(){
-    local msg=$1
-    log "${msg}" ERROR
+function logErr() {
+  local msg=$1
+  log "${msg}" ERROR
 }
 
-function logInfo(){
-    local msg=$1
-    log "${msg}" INFO
+function logInfo() {
+  local msg=$1
+  log "${msg}" INFO
 }
 
-function is_installed(){
-    cmd=$1
-    command -v "${cmd}" &> /dev/null
+function is_installed() {
+  cmd=$1
+  command -v "${cmd}" &>/dev/null
 }
 
-function install_aws(){
-    if is_installed aws;then
-        logInfo "aws cli is already installed so skip install"
-        return 0
-    fi
-    logInfo "install aws cli"
-    apt-get update || return 1
-    apt-get install unzip || return 1
-    local url="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
-    curl --fail "${url}" -o "awscliv2.zip" || return 1
-    unzip -q awscliv2.zip || return 1
-    rm -f awscliv2.zip
-    ./aws/install || return 1
-    rm -rf ./aws
+function install_aws() {
+  if is_installed aws; then
+    logInfo "aws cli is already installed so skip install"
+    return 0
+  fi
+  logInfo "install aws cli"
+  apt-get update || return 1
+  apt-get install unzip || return 1
+  local url="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+  curl --fail "${url}" -o "awscliv2.zip" || return 1
+  unzip -q awscliv2.zip || return 1
+  rm -f awscliv2.zip
+  ./aws/install || return 1
+  rm -rf ./aws
 }
 
-function install_tailscale(){
-    if is_installed tailscaled; then
-        logInfo "tailscale is already installed so skip install"
-        return 0
-    fi
-    logInfo "install tailscale"
-    curl --fail -o tailscale-install.sh --max-time 20 https://tailscale.com/install.sh || return 1
-    chmod +x tailscale-install.sh
-    ./tailscale-install.sh || return 1
-    rm -f tailscale-install.sh
+function install_tailscale() {
+  if is_installed tailscaled; then
+    logInfo "tailscale is already installed so skip install"
+    return 0
+  fi
+  logInfo "install tailscale"
+  curl --fail -o tailscale-install.sh --max-time 20 https://tailscale.com/install.sh || return 1
+  chmod +x tailscale-install.sh
+  ./tailscale-install.sh || return 1
+  rm -f tailscale-install.sh
+}
+
+function configure_tailscale_ssh() {
+  local override_file=/etc/systemd/system/tailscaled.service.d/override.conf
+  if [[ -s "${override_file}" ]]; then
+    logInfo "tailscale has already been configured to enable ssh so skipping configuration process"
+    return 0
+  fi
+
+  cat <<EOF >/etc/systemd/system/tailscaled.service.d/override.conf
+[Service]
+ExecStartPost=/usr/local/bin/aws-ec2-scripts/tailscale/tailscale-init-ssh.sh
+EOF
+  systemctl daemon-reload
+  systemctl restart tailscaled
 }
 
 if ! install_aws; then
-    logErr "failed to install the aws cli"
-    exit 1
+  logErr "failed to install the aws cli"
+  exit 1
 fi
 
 if ! install_tailscale; then
-    logErr "failed to install tailscale"
-    exit 1
+  logErr "failed to install tailscale"
+  exit 1
 fi
 
+if ! configure_tailscale_ssh; then
+  logErr "failed to configure tailscale to run a ssh server"
+  exit 1
+fi
--- tailscale/tailscale-init-ssh.sh
+++ tailscale/tailscale-init-ssh.sh
@@ -1,28 +1,58 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2181
 
 set -u
 set -o pipefail
 
+function log() {
+  local msg=$1
+  local logLevel=$2
+  local metadata
+  metadata=$(caller 1 | awk '{printf "lineno=%s funcName=%s",$1,$2}')
+  echo "[logLevel=${logLevel} ${metadata}] ${msg}" 1>&2
+}
 
-SECRET_ARN=arn:aws:secretsmanager:us-east-2:730348807217:secret:tailscale/keys/bastion-uaRrtw
+function logErr() {
+  local msg=$1
+  log "${msg}" ERROR
+}
 
-AUTH_KEY=$(aws secretsmanager get-secret-value --secret-id "${SECRET_ARN}" --query 'SecretString' --output text)
+function get_tag_value() {
+  local tag_name
+  tag_name=$1
+  local tag_value
+  tag_value=$(aws ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" "Name=key,Values=${tag_name}" --query 'Tags[0].Value' --output text)
+  if [[ $? -ne 0 || -z "${tag_value}" ]]; then
+    logErr "for ec2 instance ${INSTANCE_ID}, failed to get value for tag ${tag_name}"
+    exit 1
+  fi
+  echo "${tag_value}"
+}
+
+METADATA_URL="http://169.254.169.254/latest"
+TOKEN=$(curl -f -s -X PUT "${METADATA_URL}/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+if [[ $? -ne 0 || -z "${TOKEN}" ]]; then
+  logErr "failed to token from AWS to authenticate to the ec2 metadata service"
+  exit 1
+fi
+
+INSTANCE_ID=$(curl -f -s -H "X-aws-ec2-metadata-token: ${TOKEN}" "${METADATA_URL}/meta-data/instance-id")
+if [[ $? -ne 0 || -z "${INSTANCE_ID}" ]]; then
+  echo "ERROR - failed to get instance id"
+  exit 1
+fi
+readonly INSTANCE_ID
+
+TAILSCALE_HOSTNAME=$(get_tag_value "tailscale-hostname") || exit 1
+TAILSCALE_AUTH_KEY_ID=$(get_tag_value "tailscale-auth-key-id") || exit 1
+
+AUTH_KEY=$(aws secretsmanager get-secret-value --secret-id "${TAILSCALE_AUTH_KEY_ID}" --query 'SecretString' --output text)
 if [[ $? -ne 0 || -z "${AUTH_KEY}" ]]; then
-    echo "ERROR - failed to get tailscale auth key"
-    exit 1
+  logErr "failed to get tailscale auth key from secrets manager with secret id ${TAILSCALE_AUTH_KEY_ID}"
+  exit 1
 fi
 
-
-TOKEN=$(curl -f -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-INSTANCE_ID=$(curl -f -s -H "X-aws-ec2-metadata-token: ${TOKEN}" http://169.254.169.254/latest/meta-data/instance-id)
-
-if [[ -z "${INSTANCE_ID}" ]]; then
-    echo "ERROR - failed to get instance id"
-    exit 1
+if ! tailscale up --ssh --auth-key "${AUTH_KEY}" --hostname "${TAILSCALE_HOSTNAME}"; then
+  logErr "failed to establish a connection to the tailscale network"
+  exit 1
 fi
-
-TAILSCALE_HOSTNAME=$(aws ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" "Name=key,Values=Tailscale-Hostname" --query 'Tags[0].Value' --output text)
-
-tailscale up --ssh --auth-key "${AUTH_KEY}" --hostname "${TAILSCALE_HOSTNAME}"
-
-
